4065b515e5e67166682147f8d06deffcfcac1397
Andre1qaz <andresaragih932@gmail.com>
2025-06-27 10:07:31 +0700

    gas

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,23 @@ const {
 
 const routes = [
   {
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      return {
+        status: 'success',
+        message: 'Bookshelf API is running!',
+        endpoints: {
+          'POST /books': 'Add new book',
+          'GET /books': 'Get all books',
+          'GET /books/{id}': 'Get book by ID',
+          'PUT /books/{id}': 'Update book',
+          'DELETE /books/{id}': 'Delete book'
+        }
+      };
+    },
+  },
+  {
     method: 'POST',
     path: '/books',
     handler: addBookHandler,
